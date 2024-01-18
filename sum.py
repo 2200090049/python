0aff2550ed9ebfc79e007914c50c947ab3f40375
2200090049 <2200090049@kluniversity.in>
2024-01-18 14:05:43 +0530

    pyhton file

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Define the two numbers to be added
+number1 = 5
+number2 = 10
+
+# Add the two numbers
+sum_result = number1 + number2
+
+# Print the result
+print(f"The sum of {number1} and {number2} is: {sum_result}")
